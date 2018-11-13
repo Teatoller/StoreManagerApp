b678365a4df7a23ec4fa53674a4d7e8d8e22d57d
Steven Ennis <Steven.otieno1@gmail.com>
2018-11-13 07:34:56 +0300

    [#161866544] Remodel login js script

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,12 +1,11 @@
-const logins = document.getElementById("loginForm");
-logins.addEventListener("submit", fetchlogin);
+let route = "http://127.0.0.1:5000/api/v2";
+document.getElementById("loginForm").addEventListener("submit",
 function fetchlogin(event){
     event.preventDefault()
     let username = document.forms["login"]["username"].value;
     let password = document.forms["login"]["password"].value;
-    let route = "http://127.0.0.1:5000/api/v2";
     if (username == ""){
-        alert("Username cannot be empty");
+        alert("Username field cannot be empty");
         document.login.username.focus();
         return false
     }
@@ -21,19 +20,19 @@ function fetchlogin(event){
     }
     let request = new Request(url, {
         method:'POST',
-        body:data,
+        body: data,
         mode:'cors',
-        headers: {
-            "Content-type": "Application/json"
+        headers:{
+            'Content-type': 'Application/json'
         },
         body:JSON.stringify(data)
     })
     fetch(request)
-    .then((res) =>res.json())
-    .then((data) => {
-        if (data["acces_token"]) {
+    .then((res)=>res.json())
+    .then((data)=> {
+        if (data["access_token"]) {
             localStorage.setItem("access_token", JSON.stringify(data["access_token"]));
-            window.location.assign("landing.html");
+            window.location.assign("landing.html")
         }
         else {
             document.getElementById("loginStatus").innerHTML = data["message"];
@@ -41,92 +40,5 @@ function fetchlogin(event){
         }
     })
     .catch(error => console.log('error', error));
-}
 
-
-// let route = "http://127.0.0.1:5000/api/v2";
-
-// document.getElementById("loginForm").addEventListener("submit",
-// function fetchlogin(event){
-//     event.preventDefault()
-//     let username = document.forms["login"]["username"].value;
-//     let password = document.forms["login"]["password"].value;
-
-//     if (username == ""){
-//         alert("Username field must not be empty");
-//         document.login.username.focus();
-//         return false
-//     }
-//     else if (password == ""){
-//         alert("Password can't be empty");
-//         document.login.username.focus();
-//         return false
-//     }
-//     else{
-//         let url = route+"/auth/login"
-//         let data = {username:username, password:password};
-        // fetch(url, {
-        //     method: "POST",
-        //     headers: {  
-        //         "Content-Type": "application/json"
-        //     },
-        //     body:JSON.stringify(data)
-        // })
-//         let request = new Request(url, {
-//             method:'POST',
-//             body:data,
-//             mode:'cors',
-//             headers:{
-//                 'Content-type': 'Application/json'
-//             },
-//             body:JSON.stringify(data)
-//         })
-//         fetch(request)            
-//     .then((res)=>res.json())
-//     .then((data) => {
-//         if (data["access_token"]) {
-//             localStorage.setItem("access_token", JSON.stringify(data["access_token"]));
-//             window.location.assign("landing.html");
-//         }
-//         else {
-//             document.getElementById("loginStatus").innerHTML = data["msg"];
-//             console.log(data["msg"])
-//         }
-//     })
-//     .catch(error => console.log('error : ', error));
-// }
-// });
-
-// const login = document.getElementById('login');
-// login.addEventListener('submit',(e) => {
-//     e.preventDefault();
-//     let username = document.getElementById('username').value;
-//     let password = document.getElementById('password').value;
-// const url = "http://127.0.0.1:5000/api/v2/auth/login";
-// let data = {
-//     username:'username',
-//     password: 'password'
-// }
-// let request = new Request(url, {
-//     method:'POST',
-//     body:'data',
-//     mode:'cors',
-//     headers:{
-//         'Accept': 'Application/json',
-//         'Content-type': 'Application/json'
-//     },
-//     body:JSON.stringify({username:username, password:password})
-// })
-// fetch(request)
-// .then(function(response){return response.json()})
-// .then(function(response){
-//     localStorage.setItem('token',response.token);
-//     if(response.message === 'invalid')
-//     {
-//         document.getElementById('error').innerHTML = response.message;
-//     }
-//     else
-//     localStorage.setItem('token', response,token)
-//     window.location.assign('login.html')
-// })
-// })
+})
