87a6e401f3ff23a531d4ba758c3d8aac621998fc
Steven Ennis <Steven.otieno1@gmail.com>
2018-11-13 07:58:21 +0300

    [#161866544] Check for unresolved syntax

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,5 +1,6 @@
-let route = "http://127.0.0.1:5000/api/v2";
-document.getElementById("loginForm").addEventListener("submit",
+const url = "http://127.0.0.1:5000/api/v2";
+const applogin = document.getElementById("loginForm");
+applogin.addEventListener("submit", fetchlogin);
 function fetchlogin(event){
     event.preventDefault()
     let username = document.forms["login"]["username"].value;
@@ -23,7 +24,8 @@ function fetchlogin(event){
         body: data,
         mode:'cors',
         headers:{
-            'Content-type': 'Application/json'
+            'Content-type': 'application/json',
+            'Accept': 'application/json,text/plan, */*'
         },
         body:JSON.stringify(data)
     })
@@ -42,3 +44,48 @@ function fetchlogin(event){
     .catch(error => console.log('error', error));
 
 })
+
+// let route = "http://127.0.0.1:5000/api/v2";
+// document.getElementById("loginForm").addEventListener("submit",
+// function fetchlogin(event){
+//     event.preventDefault()
+//     let username = document.forms["login"]["username"].value;
+//     let password = document.forms["login"]["password"].value;
+//     if (username == ""){
+//         alert("Username field cannot be empty");
+//         document.login.username.focus();
+//         return false
+//     }
+//     else if (password == ""){
+//         alert("Password cannot be empty");
+//         document.login.password.focus();
+//         return false
+//     }
+//     else{
+//         let url = route+"/auth/login"
+//         let data = {username:username, password:password};
+//     }
+//     let request = new Request(url, {
+//         method:'POST',
+//         body: data,
+//         mode:'cors',
+//         headers:{
+//             'Content-type': 'Application/json'
+//         },
+//         body:JSON.stringify(data)
+//     })
+//     fetch(request)
+//     .then((res)=>res.json())
+//     .then((data)=> {
+//         if (data["access_token"]) {
+//             localStorage.setItem("access_token", JSON.stringify(data["access_token"]));
+//             window.location.assign("landing.html")
+//         }
+//         else {
+//             document.getElementById("loginStatus").innerHTML = data["message"];
+//             console.log(data["message"])
+//         }
+//     })
+//     .catch(error => console.log('error', error));
+
+// })
