c5bde86b771c049b4bc37add4fade9c26f279b90
Steven Ennis <Steven.otieno1@gmail.com>
2018-11-15 04:13:55 +0300

    [#161866544] Correct script syntax to debug

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,10 +1,11 @@
-const url = "http://127.0.0.1:5000/api/v2";
+// const route = "http://127.0.0.1:5000/api/v2";
+const route = "https://store-manager2-api-heroku.herokuapp.com/api/v2"
 const applogin = document.getElementById("loginForm");
-applogin.addEventListener("submit", fetchlogin);
+applogin.addEventListener("submit",
 function fetchlogin(event){
     event.preventDefault()
-    let username = document.forms["login"]["username"].value;
-    let password = document.forms["login"]["password"].value;
+    let username = applogin["username"].value;
+    let password = applogin["password"].value;
     if (username == ""){
         alert("Username field cannot be empty");
         document.login.username.focus();
@@ -15,77 +16,33 @@ function fetchlogin(event){
         document.login.password.focus();
         return false
     }
-    else{
+    else{        
         let url = route+"/auth/login"
-        let data = {username:username, password:password};
+        let data = {
+            'username': username,
+            'password': password
+        };        
+        fetch(url, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json, */*'
+            },
+            mode:'cors',
+            body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        // .then((data1) => console.log(data1))
+        .then((data)=> {
+            if (data["access_token"]) {
+                localStorage.setItem("access_token", JSON.stringify(data["access_token"]));
+                window.location.assign("landing.html")
+            }
+            else {
+                document.getElementById("loginStatus").innerHTML = data["msg"];
+                console.log(data["msg"]);
+            }
+        })
+        .catch(err => console.log(err));
     }
-    let request = new Request(url, {
-        method:'POST',
-        body: data,
-        mode:'cors',
-        headers:{
-            'Content-type': 'application/json',
-            'Accept': 'application/json,text/plan, */*'
-        },
-        body:JSON.stringify(data)
-    })
-    fetch(request)
-    .then((res)=>res.json())
-    .then((data)=> {
-        if (data["access_token"]) {
-            localStorage.setItem("access_token", JSON.stringify(data["access_token"]));
-            window.location.assign("landing.html")
-        }
-        else {
-            document.getElementById("loginStatus").innerHTML = data["message"];
-            console.log(data["message"])
-        }
-    })
-    .catch(error => console.log('error', error));
-
-})
-
-// let route = "http://127.0.0.1:5000/api/v2";
-// document.getElementById("loginForm").addEventListener("submit",
-// function fetchlogin(event){
-//     event.preventDefault()
-//     let username = document.forms["login"]["username"].value;
-//     let password = document.forms["login"]["password"].value;
-//     if (username == ""){
-//         alert("Username field cannot be empty");
-//         document.login.username.focus();
-//         return false
-//     }
-//     else if (password == ""){
-//         alert("Password cannot be empty");
-//         document.login.password.focus();
-//         return false
-//     }
-//     else{
-//         let url = route+"/auth/login"
-//         let data = {username:username, password:password};
-//     }
-//     let request = new Request(url, {
-//         method:'POST',
-//         body: data,
-//         mode:'cors',
-//         headers:{
-//             'Content-type': 'Application/json'
-//         },
-//         body:JSON.stringify(data)
-//     })
-//     fetch(request)
-//     .then((res)=>res.json())
-//     .then((data)=> {
-//         if (data["access_token"]) {
-//             localStorage.setItem("access_token", JSON.stringify(data["access_token"]));
-//             window.location.assign("landing.html")
-//         }
-//         else {
-//             document.getElementById("loginStatus").innerHTML = data["message"];
-//             console.log(data["message"])
-//         }
-//     })
-//     .catch(error => console.log('error', error));
-
-// })
+});
