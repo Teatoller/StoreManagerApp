a83588cdbda3ccadc65beea5b2565396485a82d7
Steven Ennis <Steven.otieno1@gmail.com>
2018-11-19 09:39:14 +0300

    [Starts #162036215] Initial commit

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -1,18 +1,33 @@
 // const route = "http://127.0.0.1:5000/api/v2";
 const route = "https://store-manager2-api-heroku.herokuapp.com/api/v2"
 const appsignup = document.getElementById("signupForm");
-appsignup.addEventListener("submit",
-function fetchlogin(event){
-    event.preventDefault()
-    let firstname = applogin["firstname"].value;
-    let lastname = applogin["lastname"].value;
-    let username = applogin["username"].value;
-    let email = applogin["email"].value;
-    let password = applogin["password"].value;
-    let role = applogin["role"].value;
-    if (username == ""){
+appsignup.addEventListener("click","submit");
+function appsignup(event){
+    event.preventDefault();
+    let firstname = appsignup["firstname"].value;
+    let lastname = appsignup["lastname"].value;
+    let username = appsignup["username"].value;
+    let email = appsignup["email"].value;
+    let password = appsignup["password"].value;
+    let role = appsignup["role"].value;
+    if (firstname == ""){
+        alert("Firstname field cannot be empty");
+        document.login.firstname.focus();
+        return false
+    }
+    else if (lastname == ""){
+        alert("Lastname cannot be empty");
+        document.login.lastname.focus();
+        return false
+    }
+    else if (username == ""){
         alert("Username field cannot be empty");
         document.login.username.focus();
+        return false
+    }
+    else if (email == ""){
+        alert("Email cannot be empty");
+        document.login.email.focus();
         return false
     }
     else if (password == ""){
@@ -20,11 +35,20 @@ function fetchlogin(event){
         document.login.password.focus();
         return false
     }
+    else if (role == ""){
+        alert("Role cannot be empty");
+        document.login.role.focus();
+        return false
+    }
     else{        
-        let url = route+"/auth/login"
+        let url = route+"/auth/signup"
         let data = {
+            'firstname': firstname,
+            'lastname': lastname,
             'username': username,
-            'password': password
+            'email': email,
+            'password': password,
+            'role': role
         };        
         fetch(url, {
             method: 'POST',
